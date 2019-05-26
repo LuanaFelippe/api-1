2d04beae64e35905789568fa6c5a267f37be463b
LuanaFelippe <50850761+LuanaFelippe@users.noreply.github.com>
2019-05-26 19:58:30 -0300

    Update 20190519102534_users.php
    Inclusão do campo status

--- migrations/20190519102534_users.php
+++ migrations/20190519102534_users.php
@@ -10,6 +10,14 @@ class Users extends AbstractMigration
         	->addColumn('name', 'string', ['null' => false, 'limit' => 255])
             ->addColumn('username', 'string', ['null' => false, 'limit' => 255])
             ->addColumn('password', 'string', ['null' => false, 'limit' => 255])
+			->addColumn('status', 'boolean', ['default' => false,'null' => false])
             ->create(); 
+    }
+	
+	public function up()
+    {
+        // query()
+        $stmt = $this->query('SELECT name, username, status FROM users WHERE status = 1'); // returns PDOStatement
+        $rows = $stmt->fetchAll(); // returns the result as an array
     }
 }
