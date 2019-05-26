fbb02162ed7700a29d0233d1ddeaca20af778551
LuanaFelippe <50850761+LuanaFelippe@users.noreply.github.com>
2019-05-26 20:01:39 -0300

    Update ListUserHandler.php
    Inclusão do parametro nome no serviço

--- src/User/ListUserHandler.php
+++ src/User/ListUserHandler.php
@@ -7,14 +7,23 @@ use NotORM;
 class ListUserHandler
 {
     private $db;
+	private $users = array();
 
     public function __construct(NotORM $db)
     {
         $this->db = $db;
     }
 
-    public function __invoke()
+    public function __invoke(Name $name)
     {
-        return $this->db->users();
+		if(isset($_GET['name'])){
+			$name = $_GET['name'];
+			$query = "SELECT name, user, status FROM users WHERE name LIKE '%" .$name. "%' AND status = 1";
+		} else {
+			$query = "SELECT name, user, status FROM users WHERE status = 1";
+		}
+		
+		$this->users = db->executeSelectQuery($query);
+		return $this->users;
     }
 }
